4e391b33f1e7188705cbb4d263c4cc4de57c0cc5
huyat16 <122583210+huyat16@users.noreply.github.com>
2023-12-23 11:19:12 +0700

    Add files via upload

---
+++ virus tình yêu.bat
@@ -0,0 +1,60 @@
+@echo off
+chcp 65001 >nul
+@echo off
+echo. 
+echo             EM KHÔNG NÊN MỞ FILE LẠ TRÊN MÁY
+timeout /t 2 >nul
+@echo off
+echo                           _  _
+timeout /t 1 >nul
+echo                    _  _ /` \/ `\ _  
+timeout /t 1 >nul_
+echo             _  _ /` \/ `\      /` \/ `\ _  _
+timeout /t 1 >nul
+echo           /` \/ `\      /'.  .'\      /` \/ `\
+timeout /t 1 >nul
+echo           \      /'.  .'   \/   '.  .'\      /
+timeout /t 1 >nul
+echo         _  ;.  .'   \/            \/   '.  .;  _
+timeout /t 1 >nul
+echo       /` \/ `\/                          \/` \/ `\
+timeout /t 1 >nul
+echo       \      /         LOVE YOU !!!       \      /
+timeout /t 1 >nul
+echo        '.  .;  _                        _  ;.  .'
+timeout /t 1 >nul
+echo          \/` \/ `\ _  _          _  _ /` \/ `\/
+timeout /t 1 >nul
+echo           \      /` \/ `\ _  _ /` \/ `\      /
+timeout /t 1 >nul
+echo            '.  .'\      /` \/ `\      /'.  .'
+timeout /t 1 >nul
+echo              \/   '.  .'\      /'.  .'   \/
+timeout /t 1 >nul
+echo                     \/   '.  .'   \/
+timeout /t 1 >nul
+echo                            \/
+timeout /t 1 >nul
+echo. 
+@echo off
+echo Msgbox  "Anh", vbCritical +  vbOKCancel, "virus tinh yeu"  > %temp%\file20.vbs
+echo Msgbox  "s" ^& ChrW(^&H1EBD^) ^& " "^& "v" ^& ChrW(^&H1B0^) ^& ChrW(^&H1EE3^) ^& "t", vbCritical +  vbOKCancel, "virus tinh yeu" > %temp%\file21.vbs
+echo Msgbox "m" ^& ChrW(^&H1ECD^) ^&"i" ^& " " ^& "t" ^& ChrW(^&H1B0^) ^& ChrW(^&H1EDD^) ^& "ng" ^& " " ^& "l" ^& ChrW(^7917^) ^& "a", vbCritical +  vbOKCancel, "virus tinh yeu" > %temp%\file22.vbs
+echo Msgbox ChrW(^273^) ^& ChrW(^7875^) ^& " " ^& "x" ^& ChrW(^&H00E2^) ^& "m" ^& " " ^&  "nh" ^&  ChrW(^7853^) ^& "p", vbCritical +  vbOKCancel, "virus tinh yeu" > %temp%\file23.vbs
+echo Msgbox "tr" ^& ChrW(^&H00E1^) ^& "i" ^& " " ^& "tim" ^& " " ^& "em", vbCritical +  vbOKCancel, "virus tinh yeu" > %temp%\file24.vbs
+echo Msgbox "           <3", vbCritical +  vbOKCancel, "virus tinh yeu" > %temp%\file25.vbs
+echo Msgbox "         Xem n" ^& ChrW(^&H00E8^), vbExclamation +  vbOKCancel, "virus tinh yeu" > %temp%\file26.vbs
+%temp%\file25.vbs | %temp%\file24.vbs | %temp%\file23.vbs | %temp%\file22.vbs | %temp%\file21.vbs | %temp%\file20.vbs
+%temp%\file26.vbs
+timeout /t 2 >nul
+del %temp%\file20.vbs
+del %temp%\file21.vbs
+del %temp%\file22.vbs
+del %temp%\file23.vbs
+del %temp%\file24.vbs
+del %temp%\file25.vbs
+del %temp%\file26.vbs
+start chrome https://drive.google.com/file/d/1i0mIE5iD8kdvdUaQYfWVhi5AMPX6dAXw/view?usp=sharing
+
+
+pause >nul
